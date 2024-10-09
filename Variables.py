eeee5838c6de789ac6f4b6896f5eddfd3a007c42
TanyaRo25 <rogulenkominskialeks@gmail.com>
2024-10-09 15:01:41 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,5 @@
+total_tasks=12
+hours_spent=1.5
+name_of_course='Python'
+average_value=hours_spent/total_tasks
+print("Курс:",name_of_course,", всего задач:",total_tasks,", затрачено часов:",hours_spent,", среднее время выполнения:",average_value,"часа.")
